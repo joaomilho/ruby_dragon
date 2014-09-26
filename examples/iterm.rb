8b6f06d0165bdbff60f5cfc7c4db4e1931f0f85d
Juan Lulkin <juan.lulkin@klarna.com>
2014-09-26 20:24:13 +0300

    /Users/jm/p/ruby_dragon test

--- examples/iterm.rb
+++ examples/iterm.rb
@@ -1,1 +1,9 @@
+require 'bundler/setup'
+Bundler.setup
+require 'ruby_dragon'
+
+include RubyDragon
+
+mvim = App.from_yaml("examples/iterm.yml")
+mvim.save("export/iterm.commandstext")
 
--- examples/mvim.rb
+++ examples/mvim.rb
@@ -5,7 +5,4 @@ require 'ruby_dragon'
 include RubyDragon
 
 mvim = App.from_yaml("examples/mvim.yml")
-
-#puts mvim.to_xml
-
-mvim.save("mvim.commandstext")
+mvim.save("export/mvim.commandstext")
